f0a3281ec3ea75e89fa292e4e119831ae433a392
Jason Fanger <jasonfanger6695@gmail.com>
2025-02-27 14:16:28 -0800

    Continued work on adding thruster mixing math but paused to work on redoing the thruster system

--- topside/rovs/spike_6m/kinematics.py
+++ topside/rovs/spike_6m/kinematics.py
@@ -2,8 +2,8 @@ import math
 
 import simple_pid
 
+import enums
 from config.kinematics import KinematicsConfig
-from rov_config import ROVConfig
 
 from utilities.vector import Vector3
 
@@ -82,30 +82,105 @@ class Kinematics:
     def get_pid_values(self) -> dict[str, dict[str, float]]:
         pass
 
-    def calculate_direction_mixing(self, current_heading: Vector3, target_lateral: Vector3) -> Vector3:
-        """Calculate the direction mixing for the ROV.
+    @classmethod
+    def rotate_target_lateral_movement(cls, ch: Vector3, tl: Vector3) -> Vector3:
+        """Calculate the combination of directions the thrusters need to push to move the ROV in the desired direction
+        while taking into account the current heading of the ROV. Does not account for rotating the ROV.
 
         Args:
-            current_heading (Vector3):
+            ch (Vector3):
                 The current heading of the ROV.
-            target_lateral (Vector3):
+            tl (Vector3):
                 The target lateral movement of the ROV.
 
         Returns:
             Vector3:
-                The lateral values to move the ROV in plus the  modulated by the current heading.
+                The lateral values to move the ROV modulated by the current heading.
         """
-        up = self.depth_pid.
-
-        # TODO: Verify that this actually makes sense.
-        up_dir: Vector3 = Vector3(
-            x=math.sin(current_heading.pitch),  # ROV Forward
-            y=math.sin(current_heading.roll),  # ROV Right
-            z=math.cos(current_heading.pitch)*math.cos(current_heading.roll),  # ROV Up
+        # TODO: Test this.
+        translated_lateral = Vector3(
+            x=(
+                tl.x * math.cos(ch.roll)
+                + tl.y * math.sin(ch.roll) * math.sin(ch.pitch)
+                + tl.z * math.sin(ch.roll) * math.cos(ch.pitch)
+            ),
+            y=(
+                tl.y * math.cos(ch.pitch)
+                - tl.z * math.sin(ch.pitch)
+            ),
+            z=(
+                -tl.x * math.sin(ch.roll)
+                + tl.y * math.cos(ch.roll) * math.sin(ch.pitch)
+                + tl.z * math.cos(ch.roll) * math.cos(ch.pitch)
+            ),
         )
 
-        return Vector3(
-            x=up_dir.x + target_lateral.x,
-            y=up_dir.y + target_lateral.y,
-            z=up_dir.z + target_lateral.z,
+        # # up = self.depth_pid._last_output
+        #
+        # # TODON'T: Verify that this actually makes sense.
+        # up_dir: Vector3 = Vector3(
+        #     x=math.sin(current_heading.pitch),  # ROV Forward
+        #     y=math.sin(current_heading.roll),  # ROV Right
+        #     z=math.cos(current_heading.pitch)*math.cos(current_heading.roll),  # ROV Up
+        # )
+        #
+        # return Vector3(
+        #     x=up_dir.x + tl.x,
+        #     y=up_dir.y + tl.y,
+        #     z=up_dir.z + tl.z,
+        # )
+
+        return translated_lateral
+
+    # TODO: Test and work on this to make it more efficient.
+    def mix_directions(self, heading: Vector3, lateral_target: Vector3, rotational_target: Vector3,
+                       pid_impulses: dict[str, float]) -> dict[str, float]:
+        """Mix the thrusters to move the ROV in the desired direction.
+
+        Args:
+            heading (Vector3):
+                The current heading of the ROV.
+            lateral_target (Vector3):
+                The target lateral movement of the ROV. Do not include anything PID-controlled.
+            rotational_target (Vector3):
+                The target rotational movement of the ROV. Do not include anything PID-controlled.
+            pid_impulses (dict[str, float]):
+                The PID-controlled values for the directions.
+
+        Returns:
+            dict[str, float]:
+                The output values for the thrusters.
+        """
+        directional_values: dict[str, float] = {name: 0 for name in enums.Directions}
+
+        lateral: Vector3 = self.rotate_target_lateral_movement(heading, lateral_target)
+
+        directional_values[enums.Directions.FORWARDS] = lateral.x
+        directional_values[enums.Directions.RIGHT] = lateral.y
+        directional_values[enums.Directions.UP] = lateral.z
+
+        directional_values[enums.Directions.YAW] = rotational_target.yaw
+        directional_values[enums.Directions.PITCH] = rotational_target.pitch
+        directional_values[enums.Directions.ROLL] = rotational_target.roll
+
+        for direction, value in pid_impulses.items():
+            directional_values[direction] += value
+
+        normalized_dir_vals = {}
+
+        thruster_norm = max(
+            abs(directional_values[enums.Directions.FORWARDS]),
+            abs(directional_values[enums.Directions.RIGHT]),
+            abs(directional_values[enums.Directions.UP]),
+            abs(directional_values[enums.Directions.YAW]),
+            abs(directional_values[enums.Directions.PITCH]),
+            abs(directional_values[enums.Directions.ROLL]),
         )
+
+        if thruster_norm > 1:
+            for direction, value in directional_values.items():
+                normalized_dir_vals[direction] = value / thruster_norm
+        else:
+            normalized_dir_vals = directional_values
+
+        return normalized_dir_vals
