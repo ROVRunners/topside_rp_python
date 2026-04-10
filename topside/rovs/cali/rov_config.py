4bdd2635d97b848b8ce2ea0e777229a52e83ade2
Jason Fanger <jasonfanger6695@gmail.com>
2026-04-09 20:17:08 -0700

    temp undoing to fix issues

--- topside/rovs/cali/rov_config.py
+++ topside/rovs/cali/rov_config.py
@@ -90,12 +90,12 @@ class ROVConfig:
             ThrusterPositions.FRONT_LEFT: Vector3(
                 # Left rotation is positive in all of the following cases.
                 yaw= -30,   # Set by viewing the ROV from the top, looking down. 0 degrees is the front of the ROV.
-                pitch=90,  # Set by viewing the ROV from its left side. 0 degrees is vertical up.
+                pitch=0,  # Set by viewing the ROV from its left side. 0 degrees is vertical up.
                 roll= 90    # Set by viewing the ROV from its rear. 0 degrees is vertical up.
             ),
-            ThrusterPositions.FRONT_RIGHT:    Vector3(yaw=  30, pitch= 90, roll= -90),
-            ThrusterPositions.REAR_LEFT:      Vector3(yaw=-150, pitch=-90, roll=  90),
-            ThrusterPositions.REAR_RIGHT:     Vector3(yaw= 150, pitch=-90, roll= -90),
+            ThrusterPositions.FRONT_RIGHT:    Vector3(yaw=  30, pitch= 0, roll= -90),
+            ThrusterPositions.REAR_LEFT:      Vector3(yaw=-150, pitch=0, roll=  90),
+            ThrusterPositions.REAR_RIGHT:     Vector3(yaw= 150, pitch=0, roll= -90),
             ThrusterPositions.FRONT_VERTICAL: Vector3(yaw=   0, pitch=  0, roll= 180),
             ThrusterPositions.REAR_VERTICAL:  Vector3(yaw=   0, pitch=  0, roll=-180),
         }
